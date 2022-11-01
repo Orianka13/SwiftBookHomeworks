a4aa5573073e162d69b9384db2e1cfaa1a22d207
Orianka13 <odobrogor@gmail.com>
2022-11-01 17:23:40 +0300

    Реализована логика слайдеров

--- ColorizedApp/ColorizedApp/ViewController.swift
+++ ColorizedApp/ColorizedApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
     }
-
-
+    
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        setColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        setColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        setColor()
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue:  CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
