11971f807ff85b007529ac5fb197e84848b3c485
Orianka13 <odobrogor@gmail.com>
2022-11-02 10:40:01 +0300

    Рефакторинг

--- ColorizedApp/ColorizedApp/ViewController.swift
+++ ColorizedApp/ColorizedApp/ViewController.swift
@@ -22,10 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue:  CGFloat(blueSlider.value),
-                                            alpha: 1)
+        setColor()
         
         redLabel.text = String(format: "%.2f", redSlider.value)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
