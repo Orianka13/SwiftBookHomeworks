536dca0fdc01f995ce1238966fc824126e5b43a5
Orianka13 <odobrogor@gmail.com>
2022-11-28 11:37:08 +0400

    Добавлен WelcomeVC

--- LoginApp/LoginApp/WelcomeViewController.swift
+++ LoginApp/LoginApp/WelcomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
