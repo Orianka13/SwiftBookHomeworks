b50ca700433173cfc6a17f73a23f2b6b206128d8
Orianka13 <odobrogor@gmail.com>
2022-11-29 11:07:31 +0400

    Исправлены баги

--- LoginApp/LoginApp/LoginViewController.swift
+++ LoginApp/LoginApp/LoginViewController.swift
@@ -12,29 +12,12 @@ final class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    @IBAction func logInTapped() {
-        if userNameTextField.text == "User", passwordTextField.text == "123" {
-            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
-        } else {
-            showAlert(title: "Error", message: "User name or password is incorrect")
-            passwordTextField.text = ""
-        }
-    }
+    private var userName = "User"
+    private var password = "123"
     
-    @IBAction func forgotUserName() {
-        showAlert(title: "Oops!", message: "Your name is User")
-    }
-    
-    
-    @IBAction func forgotPassword() {
-        showAlert(title: "Oops!", message: "Your password is 123")
-    }
-    
-    private func showAlert(title: String, message: String){
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ok", style: .default)
-        alert.addAction(okAction)
-        self.present(alert, animated: true)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -43,14 +26,33 @@ final class LoginViewController: UIViewController {
         }
     }
     
+    @IBAction func logInTapped() {
+        if userNameTextField.text == userName, passwordTextField.text == password {
+            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+        } else {
+            showAlert(title: "Error", message: "User name or password is incorrect")
+            passwordTextField.text = ""
+        }
+    }
+    
+    @IBAction func forgotUserName() {
+        showAlert(title: "Oops!", message: "Your name is \(userName)")
+    }
+    
+    
+    @IBAction func forgotPassword() {
+        showAlert(title: "Oops!", message: "Your password is \(password)")
+    }
+    
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-        guard segue.source is WelcomeViewController else { return }
         userNameTextField.text = ""
         passwordTextField.text = ""
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super .touchesBegan(touches, with: event)
-        self.view.endEditing(true)
+    private func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
